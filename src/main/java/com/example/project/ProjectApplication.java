641f7256dbc5e4580a5123e8b92d58f705cace08
eulyoung jung <81278853+jungeulyoung@users.noreply.github.com>
2024-05-31 03:13:18 +0900

    first commit

---
+++ src/main/java/com/example/project/ProjectApplication.java
@@ -0,0 +1,15 @@
+package com.example.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApplication {
+
+    public static void main(String[] args) {
+        System.out.println("dkjfkdjfkj");
+
+        SpringApplication.run(ProjectApplication.class, args);
+    }
+
+}
